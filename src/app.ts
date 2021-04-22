59e9db30332b2dd785177369e8e9705af86d0793
Joao flavio <flaviosenne123@gmail.com>
2021-04-22 08:54:05 -0300

    connectio websocket with client html

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
 import express from 'express'
+import { createServer}from 'http'
+import { Server, Socket}from 'socket.io'
 import { routes} from './routes'
+import path from 'path'
 import './db'
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+app.set('views', path.join(__dirname, '..', 'public', 'html'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+
+app.get('/pages/client', (req, res) => {
+    res.render('client')
+})
+
+const http = createServer(app)
+const io = new Server(http)
+
+io.on('connection', (soccket: Socket) => {
+    console.log('conectou ', soccket.id)
+})
+
 app.use(express.json())
 app.use(routes)
 
-export { app }
+export { http, io }
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
-import { app as server} from './app'
+import { http as server} from './app'
+import './websocket/client'
 
 server.listen(3000, ()=> console.info('server running in port 3000'))
